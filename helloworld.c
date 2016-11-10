9419bce8bd9da86c652097291b099e08d84231a7
Yedam Kim <dpeka0328@naver.com>
2016-11-10 10:57:11 +0900

    helloworld.c modified on Local repository

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,12 @@
+//For command line git tutorial
+//Local repository modification
 #include <stdio.h>
 
 int main(void){
 	printf("Hello World\n");
 	printf("Hello Your World\n");
 	printf("Hello His World\n");
-	printf("Hello Her World\n");
+	printf("Hello her World\n");
+
 	return 0;
 }
