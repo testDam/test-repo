59edeb67f0b7f900f2ec685fd017fd384b4ce436
Yedam Kim <dpeka0328@naver.com>
2016-11-26 18:36:04 +0900

    add "ls"

--- testsh.c
+++ testsh.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 #define MAX_PATH	100
 #define LF			10
@@ -16,6 +19,27 @@ int print_prompt()
 						return 0;
 }
 
+//ls 명령 실행 (명령 인자 X)
+int ls_dir(){
+	DIR *dp;
+	struct dirent *dent;
+	struct stat sbuf;
+	char dirname[BUF_SIZE] = {'\0'};
+	char path[BUF_SIZE];
+
+	if((dp = opendir("./")) == NULL){
+			perror("opendir error");
+			exit(1);
+	}
+	while ((dent = readdir(dp))){
+		strcpy(dirname, dent->d_name);
+		printf("%s	", dirname);
+	}
+	closedir(dp);
+	
+	return 0;
+}
+
 int main()
 {
 		char str[BUF_SIZE]={'\0'};
@@ -24,6 +48,10 @@ int main()
 			print_prompt();
 			fgets(str, BUF_SIZE, stdin);
 			if(!strcmp(str, "exit\n")) exit(0);
+			if(!strcmp(str, "ls\n")){
+				ls_dir();
+				printf("\n");
+			}
 			
 		}
 
